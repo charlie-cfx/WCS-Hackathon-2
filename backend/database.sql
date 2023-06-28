dd08c684032eec272c87823131fe7be300e9473e
NGBANDA_JUNIOR <njjngbanda20@yahoo.com>
2023-06-28 14:14:00 +0200

    correction database.sql

--- backend/database.sql
+++ backend/database.sql
@@ -6,16 +6,6 @@
 
 -- MySQL Workbench Forward Engineering
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-
-SET
-    @OLD_FOREIGN_KEY_CHECKS = @ @FOREIGN_KEY_CHECKS,
-    FOREIGN_KEY_CHECKS = 0;
-
-SET
-    @OLD_SQL_MODE = @ @SQL_MODE,
-    SQL_MODE = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
 -- -----------------------------------------------------
 
 -- Schema emmaus_phone
@@ -79,6 +69,7 @@ CREATE TABLE
         `weighting` INT NOT NULL,
         PRIMARY KEY (`id`)
     ) ENGINE = InnoDB;
+
 
 -- -----------------------------------------------------
 
@@ -188,9 +179,3 @@ CREATE TABLE
         `answer` VARCHAR(255) NOT NULL,
         PRIMARY KEY (`id`)
     ) ENGINE = InnoDB;
-
-SET SQL_MODE=@OLD_SQL_MODE;
-
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
