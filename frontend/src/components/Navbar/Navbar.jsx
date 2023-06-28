7ea9d9f003e9b1282c87cc5d1f3ef45a04a10b4a
Charlie FEIX <contact@charliefeix.fr>
2023-06-28 23:56:07 +0200

    Navbar done

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,101 @@
+import "./Navbar.scss";
+
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+export default function Navbar() {
+  const navigate = useNavigate();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <>
+      <nav id="navbar">
+        <div className="logo">
+          <img src="../src/assets/Logo.svg" alt="Logo Emmaus Connect" />
+        </div>
+        <ul className="items">
+          <li>
+            <button className="button-md-blue-solid" type="button">
+              <i className="fi fi-rr-plus" /> Ajouter un smartphone
+            </button>
+          </li>
+          <li>
+            <button
+              className="button-md-grey-outline"
+              onClick={() => navigate("/faq")}
+              type="button"
+            >
+              <i className="fi fi-rr-interrogation" /> FAQ
+            </button>
+          </li>
+          <li>
+            <button className="button-md-grey-outline" type="button">
+              <i className="fi fi-rr-tags" /> Gérer la pondération
+            </button>
+          </li>
+          <li>
+            <button
+              className="button-md-grey-outline"
+              type="button"
+              onClick={() => navigate("/user")}
+            >
+              <i className="fi fi-rr-user" /> Mon compte
+            </button>
+          </li>
+        </ul>
+      </nav>
+      <div id="navbar-mobile">
+        <nav>
+          <div className="logo">
+            <img src="../src/assets/Logo.svg" alt="Logo Emmaus Connect" />
+          </div>
+
+          <button
+            type="button"
+            className="burger"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            {isMenuOpen ? (
+              <i className="fi fi-rr-cross" />
+            ) : (
+              <i className="fi fi-rr-menu-burger" />
+            )}
+          </button>
+        </nav>
+        {isMenuOpen && (
+          <ul className="items">
+            <li>
+              <button className="button-md-blue-solid" type="button">
+                <i className="fi fi-rr-plus" /> Ajouter un smartphone
+              </button>
+            </li>
+
+          <li>
+            <button
+              className="button-md-grey-outline"
+              onClick={() => navigate("/faq")}
+              type="button"
+            >
+              <i className="fi fi-rr-interrogation" /> FAQ
+            </button>
+          </li>
+            <li>
+              <button className="button-md-grey-outline" type="button">
+                <i className="fi fi-rr-tags" /> Gérer la pondération
+              </button>
+            </li>
+            <li>
+              <button
+                className="button-md-grey-outline"
+                onClick={() => navigate("/user")}
+                type="button"
+              >
+                <i className="fi fi-rr-user" /> Mon compte
+              </button>
+            </li>
+          </ul>
+        )}
+      </div>
+    </>
+  );
+}
