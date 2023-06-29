9b7b3410960e95765bdca631def921489f5f713b
MatthieuGuinet <matthieuguinet@gmail.com>
2023-06-29 10:21:41 +0200

    databaseCompletion example added

--- backend/database.sql
+++ backend/database.sql
@@ -202,3 +202,143 @@ VALUES (
         '3288378372',
         1
     );
+
+INSERT INTO
+    `accessory`(name, weighting)
+VALUES ('Aucun', 0), ('Chargeur', 0), ('Ecouteur', 0), ('Verre trempé', 0), ('Chargeur + Ecouteur', 0), ('Chargeur + Verre trempé', 0), ('Ecouteur + Verre trempé', 0), (
+        'Chargeur + Ecouteur + Verre trempé',
+        0
+    );
+
+INSERT INTO `brand`
+VALUES (1, 'Apple', 0), (2, 'Samsung', 0), (3, 'Huawei', 0), (4, 'Xiaomi', 0), (5, 'OnePlus', 0), (6, 'Google', 0), (7, 'Sony', 0), (8, 'LG', 0), (9, 'Motorola', 0), (10, 'Nokia', 0), (11, 'Oppo', 0), (12, 'Vivo', 0), (13, 'Realme', 0), (14, 'HTC', 0), (15, 'Asus', 0), (16, 'Blackberry', 0), (17, 'Alcatel', 0), (18, 'ZTE', 0), (19, 'Meizu', 0);
+
+INSERT INTO `color`
+VALUES (1, 'black', 'noir'), (2, 'white', 'blanc'), (3, 'silver', 'argent'), (4, 'gray', 'gris'), (5, 'gold', 'or'), (6, 'rose', 'rose'), (7, 'blue', 'bleu'), (8, 'red', 'rouge'), (9, 'green', 'vert'), (10, 'purple', 'violet'), (11, 'yellow', 'jaune'), (12, 'blue sky', 'bleu ciel'), (13, 'bronze', 'bronze'), (14, 'brown', 'marron');
+
+INSERT INTO `model`
+VALUES (
+        1,
+        'Galaxy S23',
+        '5.8',
+        '14.5',
+        0,
+        2,
+        2
+    ), (
+        2,
+        'Galaxy S10e',
+        '5.8',
+        '14.5',
+        0,
+        2,
+        5
+    ), (
+        3,
+        'Reno6',
+        '6.4',
+        '16.2',
+        0,
+        11,
+        8
+    ), (
+        5,
+        'iPhone12',
+        '6.1',
+        '15.4',
+        0,
+        1,
+        3
+    ), (
+        6,
+        'iPhone 7',
+        '6.1',
+        '15.4',
+        0,
+        1,
+        7
+    ), (
+        7,
+        'iPhone13',
+        '6.1',
+        '15.4',
+        0,
+        1,
+        8
+    ), (
+        8,
+        'Galaxy S21',
+        '6.2',
+        '15.7',
+        0,
+        2,
+        3
+    ), (
+        9,
+        'Pixel 6',
+        '6.4',
+        '16.2',
+        0,
+        6,
+        14
+    ), (
+        10,
+        '9 Pro',
+        '6.7',
+        '17.1',
+        0,
+        5,
+        13
+    ), (
+        11,
+        'Mi 11',
+        '6.81',
+        '17.2',
+        0,
+        4,
+        11
+    ), (
+        12,
+        'P 40Pro',
+        '6.58',
+        '16.7',
+        0,
+        3,
+        9
+    ), (
+        13,
+        'Xperia III',
+        '6.5',
+        '16.5',
+        0,
+        7,
+        5
+    ), (
+        14,
+        'Velvet',
+        '6.8',
+        '17.2',
+        0,
+        8,
+        4
+    ), (
+        15,
+        'Find X3 Pro',
+        '6.7',
+        '17.1',
+        0,
+        11,
+        7
+    );
+
+INSERT INTO `os_version`
+VALUES (1, '12'), (2, '13'), (3, '1.0'), (4, '2.0');
+
+INSERT INTO `os`
+VALUES (1, 'Android', 0, 1), (2, 'iOS', 0, 2), (3, 'Windows10 mobile', 0, 3), (4, 'HarmonyOS', 0, 4);
+
+INSERT INTO `state`
+VALUES (1, 'DEEE', 0), (2, 'Réparable', 0), (3, 'Bloqué', 0), (4, 'Reconditionnable', 0), (5, 'Reconditionné', 0);
+
+INSERT INTO `phone`
+VALUES (1, 2, 1, 1, 8, 0, '4G', 2, 5), (2, 2, 2, 1, 8, 16, '4G', 5, 4), (3, 1, 5, 2, 16, 32, '5G', 4, 5), (4, 4, 11, 1, 16, 48, '5G', 1, 3), (5, 6, 9, 1, 8, 4, '3G', 3, 3);
