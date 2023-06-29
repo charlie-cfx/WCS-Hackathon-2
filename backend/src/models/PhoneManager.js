a663dff91cd2cbb55f151697baae75ede72d9b9e
MatthieuGuinet <matthieuguinet@gmail.com>
2023-06-29 09:27:57 +0200

    route filtered done this time in router

--- backend/src/models/PhoneManager.js
+++ backend/src/models/PhoneManager.js
@@ -9,31 +9,39 @@ class PhoneManager extends AbstractManager {
     const query = `SELECT * FROM ${this.table}`;
     const nbOfKeys = keys.length;
     let filters = " WHERE ";
+    const dependencies = [];
     if (nbOfKeys) {
       if (Array.isArray(valueQuery[0])) {
         const nbOfValuesForFirstKey = valueQuery[0].length;
-        filters += `${keys[0]} = ${valueQuery[0][0]}`;
+        filters += `(? = ${valueQuery[0][0]}`;
+        dependencies.push(keys[0]);
         for (let k = 1; k < nbOfValuesForFirstKey; k += 1) {
-          filters += ` OR ${keys[0]} = ${valueQuery[0][k]}`;
+          filters += ` OR ? = ${valueQuery[0][k]}`;
+          dependencies.push(keys[0]);
         }
-      } else {
-        filters += `${keys[0]} = ${valueQuery[0][0]}`;
+        filters += `)`;
+      } else if (!Array.isArray(valueQuery[0])) {
+        filters += `? = ${valueQuery[0][0]}`;
+        dependencies.push(keys[0]);
       }
       if (nbOfKeys > 1) {
         for (let j = 1; j < nbOfKeys; j += 1) {
           if (!Array.isArray(valueQuery[j])) {
-            filters += ` AND ${keys[j]} = ${valueQuery[j]}`;
+            filters += ` AND ? = ${valueQuery[j]}`;
+            dependencies.push(keys[j]);
           } else {
             const nbOfValues = valueQuery[j].length;
-            filters += ` AND (${keys[j]} = ${valueQuery[0]}`;
+            filters += ` AND (? = ${valueQuery[j][0]}`;
+            dependencies.push(keys[j]);
             for (let i = 1; i < nbOfValues; i += 1) {
-              filters += ` OR ${keys[j]} = ${valueQuery[j][i]}`;
+              filters += ` OR ? = ${valueQuery[j][i]}`;
+              dependencies.push(keys[j]);
             }
             filters += `)`;
           }
         }
       }
-      return this.database.query(query + filters, [...keys]);
+      return this.database.query(query + filters, [...dependencies]);
     }
     return this.database.query(query);
   }
