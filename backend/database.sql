22e7a0ee1f730af1d31106ea63c26e7d94ad7f56
NGBANDA_JUNIOR <njjngbanda20@yahoo.com>
2023-07-02 16:15:57 +0200

    database update

--- backend/database.sql
+++ backend/database.sql
@@ -307,3 +307,26 @@ VALUES (1, 'DEEE', 100), (2, 'Réparable', 50), (3, 'Bloqué', 10), (4, 'Recondi
 
 INSERT INTO `phone`
 VALUES (1, 2, 1, 1, 8, 128, '5G', 2, 5, 620), (2, 2, 2, 1, 8, 128, '4G', 5, 4, 170), (3, 1, 5, 2, 16, 128, '5G', 4, 5, 700), (4, 4, 11, 1, 16, 256, '5G', 1, 3, 500), (5, 6, 9, 1, 8, 128, '5G', 3, 3, 420);
+
+INSERT INTO
+    `faq` VALUE (
+        1,
+        "Qu'est-ce qu'un smartphone reconditionné ?",
+        "Un smartphone reconditionné est un appareil qui a été préalablement utilisé, puis remis à neuf pour être vendu à nouveau. Il a subi des tests, des réparations éventuelles et a été remis en état de fonctionnement. Les smartphones reconditionnés offrent une alternative moins coûteuse par rapport aux nouveaux modèles, tout en garantissant une qualité satisfaisante."
+    ), (
+        2,
+        "Quelles sont les différences entre un smartphone reconditionné et un smartphone neuf ?",
+        "`La principale différence réside dans le fait qu'un smartphone reconditionné a déjà été utilisé, tandis qu'un smartphone neuf est tout juste sorti de l'emballage et n'a jamais été utilisé. Les smartphones reconditionnés peuvent présenter de légères marques d'usure, mais ils sont généralement testés et réparés pour assurer leur bon fonctionnement, tout comme les smartphones neufs."
+    ), (
+        3,
+        "Comment savoir si un smartphone est reconditionnable ?",
+        "La reconditionnabilité d'un smartphone dépend de son état initial et de la disponibilité des pièces de rechange nécessaires. Un professionnel peut évaluer l'appareil et déterminer s'il est possible de le reconditionner en effectuant les réparations nécessaires. Cependant, il est important de noter que tous les smartphones ne sont pas reconditionnables, surtout s'ils sont très anciens ou s'ils présentent des dommages irréparables."
+    ), (
+        4,
+        "Qu'est-ce qu'un smartphone bloqué ?",
+        "Un smartphone bloqué fait référence à un appareil qui est verrouillé sur un seul réseau ou opérateur spécifique. Cela signifie que vous ne pouvez utiliser la carte SIM que de cet opérateur particulier. Pour utiliser une autre carte SIM d'un opérateur différent, vous devrez soit déverrouiller le smartphone, soit utiliser une méthode de déblocage appropriée."
+    ), (
+        5,
+        "Puis-je réparer moi-même mon smartphone ?",
+        "La réparation d'un smartphone peut varier en fonction de la complexité du problème. Certaines réparations mineures, comme le remplacement d'une batterie ou d'un écran, peuvent être effectuées par des utilisateurs expérimentés à l'aide de tutoriels en ligne et d'outils appropriés. Cependant, pour des réparations plus complexes ou pour éviter d'endommager davantage l'appareil, il est recommandé de faire appel à un professionnel de la réparation de smartphones."
+    );
